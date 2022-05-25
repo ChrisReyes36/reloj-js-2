f863d8e0a5f8f34fe08140a69cdd4802ede597f2
Christopher Muñoz <christopherreyes456@gmail.com>
2022-05-25 09:12:56 -0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const horaHTML = document.getElementById("hora");
+const minutoHTML = document.getElementById("minuto");
+const segundoHTML = document.getElementById("segundo");
+const tipoHTML = document.getElementById("tipo");
+
+const mostrarHora = () => {
+    const fecha = new Date();
+    let hora = fecha.getHours();
+    hora = hora === 0 ? 12 : hora;
+    hora = hora > 12 ? hora - 12 : hora;
+    let minuto = fecha.getMinutes();
+    let segundo = fecha.getSeconds();
+    const tipo = hora >= 12 ? "PM" : "AM";
+
+    horaHTML.textContent = String(hora).padStart(2, "0");
+    minutoHTML.textContent = String(minuto).padStart(2, "0");
+    segundoHTML.textContent = String(segundo).padStart(2, "0");
+    tipoHTML.textContent = tipo;
+
+    setTimeout(mostrarHora, 1000);
+};
+
+document.addEventListener("DOMContentLoaded", mostrarHora);
